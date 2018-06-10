2c12c701296f03a7f6195b8231dd134ac39ab537
Fiona Lapham <fionadlapham@gmail.com>
2018-06-10 08:59:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.now < 12
+    time = Time.now
       if time.hour <= 12
         resp.write "Good Morning."
     else
